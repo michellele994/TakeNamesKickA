4bb3631833bfd85d1df091a9b233338b7ed34a54
michellele994 <michellele@email.arizona.edu>
2018-02-15 01:30:47 -0700

    Changed server port from const to var

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 const app = express();
 
